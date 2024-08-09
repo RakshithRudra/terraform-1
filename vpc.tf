83ae86c71f318cfc62772e6b6d850349c23f0eb6
RakshithRudra <rudrarakshith77@gmail.com>
2024-08-09 12:17:26 +0530

    created vpc and subnet

--- vpc.tf
+++ vpc.tf
@@ -4,4 +4,35 @@ resource "aws_vpc" "java-vpc" {
 
   tags = {
     Name = "java-vpc"
+  }
+}
+
+#subnet-1
+resource "aws_subnet" "java-db" {
+  vpc_id     = aws_vpc.java-vpc.id
+  cidr_block = "10.0.0.0/18"
+
+  tags = {
+    Name = "java-db"
+  }
+}
+
+#subnet-2
+resource "aws_subnet" "java-be" {
+  vpc_id     = aws_vpc.java-vpc.id
+  cidr_block = "10.0.64.0/18"
+
+  tags = {
+    Name = "java-db"
+  }
+}
+
+#subnet-3
+resource "aws_subnet" "java-fe" {
+  vpc_id     = aws_vpc.java-vpc.id
+  cidr_block = "10.0.128.0/18"
+
+  tags = {
+    Name = "java-db"
+  }
 }
